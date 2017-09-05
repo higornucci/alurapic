69e1e1c16c3e60ba2f8ed5f169fd499c8867828d
Higor Nucci <higornucci@gmail.com>
2017-09-05 16:03:31 -0400

    criação de serviços para os verbos http

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,31 +1,26 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http) {
+angular.module('alurapic').controller('FotosController', function($scope, recursoFoto) {
 
     $scope.fotos = [];
     $scope.filtro = '';
     $scope.mensagem = '';
 
-    $http.get('/v1/fotos')
-        .success(function(retorno) {
-            console.log(retorno);
-            $scope.fotos = retorno; // não precisa fazer retorno.data
-        })
-        .error(function(erro) {
-            console.log(erro);
-        });
+    recursoFoto.query(function(fotos) {
+        $scope.fotos = fotos;
+    }, function(erro) {
+        console.log(erro);
+    });
 
     $scope.remover = function(foto) {
         console.log('remover foto:');
         console.log(foto);
-        $http.delete('v1/fotos/' + foto._id)
-            .success(function() {
-                var indiceFoto = $scope.fotos.indexOf(foto); // obtem o índice da foto que está sendo removida.
-                $scope.fotos.splice(indiceFoto, 1); // remove um elemento da lista de fotos de acordo com o índice da foto removida.
-                $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
-            })
-            .error(function(erro) {
-                console.log(erro);
-                $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
-            });
+        recursoFoto.delete({ fotoId: foto._id }, function() {
+            var indiceDaFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceDaFoto, 1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+        }, function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+        });
     };
 
 });
