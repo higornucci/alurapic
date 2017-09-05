68654feda6ae88c799313fb55e7c25050adcdf69
Higor Nucci <higornucci@gmail.com>
2017-09-05 15:38:04 -0400

    edição e exclusão de fotos

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,15 +1,31 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
-	
-	$scope.fotos = []; 
-	$scope.filtro = '';
 
-	$http.get('/v1/fotos')
-	.success(function(retorno) {
-		console.log(retorno);
-		$scope.fotos = retorno; // não precisa fazer retorno.data
-	})
-	.error(function(erro) {
-		console.log(erro);
-	});
+    $scope.fotos = [];
+    $scope.filtro = '';
+    $scope.mensagem = '';
+
+    $http.get('/v1/fotos')
+        .success(function(retorno) {
+            console.log(retorno);
+            $scope.fotos = retorno; // não precisa fazer retorno.data
+        })
+        .error(function(erro) {
+            console.log(erro);
+        });
+
+    $scope.remover = function(foto) {
+        console.log('remover foto:');
+        console.log(foto);
+        $http.delete('v1/fotos/' + foto._id)
+            .success(function() {
+                var indiceFoto = $scope.fotos.indexOf(foto); // obtem o índice da foto que está sendo removida.
+                $scope.fotos.splice(indiceFoto, 1); // remove um elemento da lista de fotos de acordo com o índice da foto removida.
+                $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+            });
+    };
 
 });
